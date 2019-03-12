ef485ebf7a20e43d2f75771c5a9c1728710a192a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-12 22:34:26 +0000

    Deploy EkinEren/BookStore to github.com/EkinEren/BookStore.git:gh-pages

---
+++ precache-manifest.e7907d7bb584edda79fc51b7cebac045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0462e65a4f90f9f7f8b",
+    "url": "/BookStore/static/css/main.50d40427.chunk.css"
+  },
+  {
+    "revision": "c0462e65a4f90f9f7f8b",
+    "url": "/BookStore/static/js/main.091e3731.chunk.js"
+  },
+  {
+    "revision": "be8ef2b8103a3d35622a",
+    "url": "/BookStore/static/js/runtime~main.f30c091c.js"
+  },
+  {
+    "revision": "ae88c3f9054048ff8689",
+    "url": "/BookStore/static/css/2.8083b315.chunk.css"
+  },
+  {
+    "revision": "ae88c3f9054048ff8689",
+    "url": "/BookStore/static/js/2.f4e45910.chunk.js"
+  },
+  {
+    "revision": "582eb88e3ecd6cb97a0ca0f55bb75cda",
+    "url": "/BookStore/index.html"
+  }
+];
